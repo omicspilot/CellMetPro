dad7130825deb484f0776a3175ecb333e70a3022
Oumar Ndiaye <96336953+dev-fun7@users.noreply.github.com>
2026-03-15 20:56:12 +0100

    clean

--- tests/data/alzheimer_cd8/tpm_counts_normalized/run_compass.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=compass_alzheimer_cd8
-#SBATCH -c 20
-#SBATCH --mem-per-cpu=4096MB
-
-compass --data ../data/linear_gene_expression_matrix.tsv --num-processes 20 --species mus_musculus --lambda 0.25 --num-neighbors 10 --output-dir lamda_met_k10 --calc-metabolites
